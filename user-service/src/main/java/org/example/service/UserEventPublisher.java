4d38c04af5fbaa7f2be0b967e67afd829c26dbd0
O6HarJleBwuunp9lHuk <anri23092003@gmail.com>
2025-10-26 17:14:56 +0300

    API с Swagger и HATEOAS

--- user-service/src/main/java/org/example/service/UserEventPublisher.java
+++ user-service/src/main/java/org/example/service/UserEventPublisher.java
@@ -1,6 +1,8 @@
 package org.example.service;
 
 import org.example.common.event.UserEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.example.common.event.EventType;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserEventPublisher {
 
+    private static final Logger log = LoggerFactory.getLogger(UserEventPublisher.class);
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private final String userEventsTopic = "user-events";
 
@@ -18,12 +21,12 @@ public class UserEventPublisher {
     public void publishUserCreated(String email) {
         UserEvent event = new UserEvent(email, EventType.USER_CREATED);
         kafkaTemplate.send(userEventsTopic, email, event);
-        System.out.println("Event published: " + event);
+        log.info("User created event published: {}", event);
     }
 
     public void publishUserDeleted(String email) {
         UserEvent event = new UserEvent(email, EventType.USER_DELETED);
         kafkaTemplate.send(userEventsTopic, email, event);
-        System.out.println("Event published: " + event);
+        log.info("User deleted event published: {}", event);
     }
 }
